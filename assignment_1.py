27b89d017f147a0f38920de482c73e18850c18a0
Vignesh Kumar Venkateshwar <45217421+vigneshkumarv@users.noreply.github.com>
2023-09-23 15:46:44 -0700

    Adding assignment 1
    Code for input and output in python

---
+++ assignment_1.py
@@ -0,0 +1,9 @@
+"""
+Assignment 1: This is a basic input and output file
+September 9, 2023
+Vignesh Kumar Venkateshwar
+"""
+
+name = input("What is your name?\n")
+know_python = input("Do you know Python?\n")
+print("Thanks for answering my questions, {}.".format(name))
